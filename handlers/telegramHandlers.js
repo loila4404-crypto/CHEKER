f99b6f1b2b0f1f7347ea7ffa25a67c35e8625620
loila4404-crypto <loila4404@gmail.com>
2026-05-19 10:26:41 +0300

    Update telegramHandlers.js

--- handlers/telegramHandlers.js
+++ handlers/telegramHandlers.js
@@ -284,7 +284,7 @@ function registerTelegramHandlers({
       );
 
       await startWaChecker({
-        phone: "checker",
+        phone: "380998338356",
         chatId: chat.id,
         bot,
         supabase,
